d31fec4556726cd91b9631c75a8dfcfacddab32a
Lygia Botelho <lygiasi@gmail.com>
2020-08-20 10:13:25 -0300

    conectando backende frontend mod2-issue8

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,38 +2,103 @@ import React, { useState } from 'react';
 import { TimeSplit } from './typings/global';
 import { tick } from './utils/time';
 import { useCssHandles } from 'vtex.css-handles';
-// import { FormattedMessage } from 'react-intl';
+import { useQuery } from 'react-apollo';
+import useProduct from 'vtex.product-context/useProduct';
+import productReleaseDate from './queries/productReleaseDate.graphql';
+
+const { product: { linkText } } = useProduct()
+const { data, loading, error } = useQuery(productReleaseDate, {
+  variables: {
+    slug: linkText
+  },
+  ssr: false,
+
+   /**
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    )
+  }
+  /**/
+})
+
+console.log({data})
 
 interface CountdownProps {
-  // title: string
   targetDate: string
 }
 const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString();
-// const CSS_HANDLES = ['countdown', "countdown", "title"];
 const CSS_HANDLES = ['countdown'];
 
-
+/**
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  // title,
   targetDate = DEFAULT_TARGET_DATE }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
     seconds: '00'
   })
+ */
 
-  // const titleText = title || <FormattedMessage id="countdown.title" />
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+   }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
   const handles = useCssHandles(CSS_HANDLES)
 
-  tick(targetDate, setTime)
+  // tick(targetDate, setTime)
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  /**/
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    )
+  }
+  /**/
 
   return (
-    // <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-    //   <div className={`${handles.title} db tc`}>{titleText}</div>
-      <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
-        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-      </div>
-    // </div>
+
+    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
+
   )
 }
 /*  <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
@@ -45,11 +110,7 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    // title: {
-    //   title: 'Sou um título',
-    //   type: 'string',
-    //   default: null,
-    // },
+
     targetDate: {
       title: 'Data final',
       description: 'Data final utilizada no contador',
